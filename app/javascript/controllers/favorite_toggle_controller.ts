044d9b3a4f676400c74e0073893bfd59a0f786a1
Jeff Sutherland <jeffdsuth@gmail.com>
2021-03-23 10:19:15 -0700

    Avoid hardcoding CSS class name using Stimulus

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -1,11 +1,13 @@
 import { Controller } from 'stimulus'
 
 export default class FavoriteToggleController extends Controller {
+  static classes = ["hidden"]
   static targets = ["elementToHide", "elementWithText"]
   static values = { visible: Boolean }
 
   elementToHideTarget: HTMLElement
   elementWithTextTarget: HTMLElement
+  hiddenClass: string
   visibleValue: boolean
 
   connect(): void {
@@ -27,7 +29,7 @@ export default class FavoriteToggleController extends Controller {
   }
 
   updateHiddenClass() {
-    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
+    this.elementToHideTarget.classList.toggle(this.hiddenClass, !this.visibleValue)
   }
 
   newText() {
