ee67e7054a32c0d42b9c53982655ff493eefd419
Jeff Sutherland <jeffdsuth@gmail.com>
2021-03-29 18:58:12 -0700

    outside click and esc dismisses search modal

--- app/javascript/controllers/search_controller.ts
+++ app/javascript/controllers/search_controller.ts
@@ -2,11 +2,31 @@ import { Controller } from 'stimulus'
 import "form-request-submit-polyfill"
 
 export default class SearchController extends Controller {
-  static targets = ["form", "input"]
+  static targets = ["form", "input", "results"]
 
   formTarget: HTMLFormElement
+  resultsTarget: HTMLFormElement
+  inputTarget: HTMLFormElement
 
   submit(): void {
     this.formTarget.requestSubmit()
+  }
+
+  resetOnOutsideClick(event: Event): void {
+    if (!this.element.contains(event.target as HTMLElement)) {
+      this.reset()
+    }
+  }
+
+  resetOnEsc(event: KeyboardEvent): void {
+    if (event.key === "Escape") {
+      this.reset()
+    }
+  }
+
+  reset(): void {
+    this.resultsTarget.classList.add("hidden")
+    this.resultsTarget.innerText = ""
+    this.inputTarget.value = ""
   }
 }
